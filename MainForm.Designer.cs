fd1f713fc7acd6c7555c9551c2b74e492d9d5ada
aztack <aztack@163.com>
2013-12-31 11:45:17 +0800

    update

---
+++ MainForm.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace Rerun
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fileSystemWatcher = new System.IO.FileSystemWatcher();
+            this.log = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // fileSystemWatcher
+            // 
+            this.fileSystemWatcher.EnableRaisingEvents = true;
+            this.fileSystemWatcher.SynchronizingObject = this;
+            this.fileSystemWatcher.Changed += new System.IO.FileSystemEventHandler(this.fileSystemWatcher_Changed);
+            // 
+            // log
+            // 
+            this.log.AllowDrop = true;
+            this.log.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.log.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.log.Location = new System.Drawing.Point(0, 0);
+            this.log.Multiline = true;
+            this.log.Name = "log";
+            this.log.Size = new System.Drawing.Size(532, 105);
+            this.log.TabIndex = 0;
+            this.log.Text = "Drop script file here. The script will be rerun every time it is updated.";
+            this.log.DragDrop += new System.Windows.Forms.DragEventHandler(this.log_DragDrop);
+            this.log.DragEnter += new System.Windows.Forms.DragEventHandler(this.log_DragEnter);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(532, 105);
+            this.Controls.Add(this.log);
+            this.Name = "MainForm";
+            this.Text = "Rerun";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.FileSystemWatcher fileSystemWatcher;
+        private System.Windows.Forms.TextBox log;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+using System.Security.Cryptography;
+using System.Threading;
+using System.Runtime.InteropServices;
+
+namespace Rerun
+{
+    public partial class MainForm : Form
+    {
+        private string ServerScript;
+        private Process proc = null;
+        private string md5OfServerScript = "";
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            fileSystemWatcher.Path = System.IO.Path.GetDirectoryName(Application.ExecutablePath);
+        }
+
+        private void fileSystemWatcher_Changed(object sender, System.IO.FileSystemEventArgs e)
+        {
+            if (ServerScript != null && e.FullPath.ToLower().IndexOf(ServerScript.ToLower()) >= 0)
+            {
+                Rerun(ServerScript);
+            }   
+        }
+
+        private void log_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop)) e.Effect = DragDropEffects.Copy;
+        }
+
+        private void log_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            string script = files[0];
+            fileSystemWatcher.Path = System.IO.Path.GetDirectoryName(script);
+            ServerScript = script;
+            log.AppendText(script + "\n");
+            Rerun(script);
+        }
+
+        const uint CTRL_C_EVENT = 0;
+        const uint CTRL_BREAK_EVENT = 1;
+
+        [DllImport("kernel32.dll")]
+        static extern bool GenerateConsoleCtrlEvent(
+            uint dwCtrlEvent,
+            uint dwProcessGroupId);
+
+        private void Rerun(string path)
+        {
+            string md5 = "";
+            try
+            {
+                md5 = GetMD5HashFromFile(path);
+            }catch(Exception){
+                return;
+            }
+            if (md5 != md5OfServerScript)
+            {
+                md5OfServerScript = md5;
+                if (proc != null)
+                {
+                    try
+                    {
+                        GenerateConsoleCtrlEvent(CTRL_C_EVENT, (uint)proc.Id);
+                        proc.Kill();
+                        proc.CloseMainWindow();
+                        log.AppendText(String.Format("Process#{0} killed\n", proc.Id));
+                    }
+                    catch (Exception e) {
+                        log.AppendText(e.Message);
+                    }
+                }
+                RunScript(path);
+            }
+        }
+
+        protected string GetMD5HashFromFile(string fileName)
+        {
+            System.IO.FileStream file = new System.IO.FileStream(fileName, FileMode.Open);
+            MD5 md5 = new MD5CryptoServiceProvider();
+            byte[] retVal = md5.ComputeHash(file);
+            file.Close();
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < retVal.Length; i++)
+            {
+                sb.Append(retVal[i].ToString("x2"));
+            }
+            return sb.ToString();
+        }
+
+        public void RunScript(string script)
+        {
+            Process process = new Process();
+
+            //process.OutputDataReceived += process_OutputDataReceived;
+            ProcessStartInfo info = new ProcessStartInfo(script);
+            //info.Arguments = String.Join(" ", arguments);
+            info.UseShellExecute = true;
+            //info.RedirectStandardError = true;
+            //info.RedirectStandardOutput = true;
+            info.WindowStyle = ProcessWindowStyle.Normal;
+            process.StartInfo = info;
+            process.EnableRaisingEvents = true;
+            process.Start();
+            //process.BeginOutputReadLine();
+            proc = process;
+            //errorMessage = process.StandardError.ReadToEnd();
+            log.AppendText(String.Format("Process#{0} created\n", proc.Id));
+        }
+        private void process_OutputDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            using (AutoResetEvent errorWaitHandle = new AutoResetEvent(false))
+            {
+                if (!string.IsNullOrEmpty(e.Data))
+                {
+                    log.AppendText(e.Data + "\n");
+                }
+            }
+        }
+    }
+}
